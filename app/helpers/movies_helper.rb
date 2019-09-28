0282f2956847b11478ad3e6a8f1a9bd23823dda4
Murtaza Hakimi <murtaza.hakimi@gmail.com>
2019-09-28 16:21:27 -0500

    part 1 complete

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,13 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+
+  # Turns on highlighting if valid field:
+  def hilite(field)
+    if(params[:sort].to_s == field)
+      return 'hilite'
+    else
+      return nil
+    end
+  end
 end
